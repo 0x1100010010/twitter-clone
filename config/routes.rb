26762bb4fd20382014e7034c5d0a0d5df9a2dc06
bashforger <bash.forger@gmail.com>
2021-01-27 20:01:16 +0500

    UI update, added tweets live feed

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :tweets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'home/index'
-  root to: 'home#index'
+  root to: 'tweets#index'
 end
